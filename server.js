514ec55e79ac4bcdced42e397e68af5b56eacdba
Pedro Henrique <phcatanduba@gmail.com>
2021-06-23 12:36:58 -0300

    sign-up update + sign-in added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
 import pg from 'pg';
+import { v4 } from 'uuid';
 
 const app = express();
 
@@ -16,31 +17,67 @@ const connection = new Pool({
     database: 'mywallet',
 });
 
-app.post('/signup', async (req, res) => {
-    const { name, email, password } = req.body;
+/*------------------ SIGN-UP-----------------*/
 
+app.post('/sign-up', async (req, res) => {
+    const { name, email, password } = req.body;
     if (!name || !email || !password) {
         res.sendStatus(400);
-    }
-    try {
-        const request = await connection.query(
-            'SELECT * FROM customers WHERE name = $1',
-            [name]
-        );
-        const hasCustomer = request.rows.length;
-        if (hasCustomer) {
-            res.sendStatus(409);
-        } else {
-            const hash = bcrypt.hashSync(password, 10);
-            connection.query(
-                'INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)',
-                [name, email, hash]
+    } else {
+        try {
+            const request = await connection.query(
+                'SELECT * FROM customers WHERE name = $1',
+                [name]
             );
-            res.sendStatus(201);
+            const hasCustomer = request.rows.length;
+            if (hasCustomer) {
+                res.sendStatus(409);
+            } else {
+                const hash = bcrypt.hashSync(password, 10);
+                await connection.query(
+                    'INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)',
+                    [name, email, hash]
+                );
+                res.sendStatus(201);
+            }
+        } catch (e) {
+            console.log(e);
+            res.sendStatus(500);
         }
-    } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
+    }
+});
+
+/*------------------ SIGN-IN-----------------*/
+
+app.get('/sign-in', async (req, res) => {
+    const { email, password, id } = req.body;
+    if (!email || !password || !id) {
+        res.sendStatus(400);
+    } else {
+        try {
+            const request = await connection.query(
+                'SELECT * FROM customers WHERE email = $1',
+                [email]
+            );
+            const customer = request.rows[0];
+            if (customer && bcrypt.compareSync(password, customer.password)) {
+                const token = v4();
+                await connection.query(
+                    `
+              INSERT INTO sessions ("customerId", token)
+              VALUES ($1, $2)
+            `,
+                    [customer.id, token]
+                );
+
+                res.send(token);
+            } else {
+                res.sendStatus(400);
+            }
+        } catch (e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
     }
 });
 
