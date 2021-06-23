e1a23f0dc6b746c782fb00454e23745bfd32d85e
Pedro Henrique <phcatanduba@gmail.com>
2021-06-23 12:07:18 -0300

    signup working

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import pg from 'pg';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const { Pool } = pg;
+const connection = new Pool({
+    user: 'postgres',
+    password: '123456',
+    host: 'localhost',
+    port: 5432,
+    database: 'mywallet',
+});
 
-app.post('/signup', (req, res) => {
-    if (!req.body) {
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
         res.sendStatus(400);
     }
-    const { name, email, password } = req.body;
+    try {
+        const request = await connection.query(
+            'SELECT * FROM customers WHERE name = $1',
+            [name]
+        );
+        const hasCustomer = request.rows.length;
+        if (hasCustomer) {
+            res.sendStatus(409);
+        } else {
+            const hash = bcrypt.hashSync(password, 10);
+            connection.query(
+                'INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)',
+                [name, email, hash]
+            );
+            res.sendStatus(201);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(4000, () => {
